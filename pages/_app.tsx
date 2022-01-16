96abe307a9e45a4de615525c0c28542b8b79a27a
Villem <maandi.villem@gmail.com>
2022-01-16 19:48:42 +0200

    added spinner at page loads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,36 @@
-import { Box, ChakraProvider, Flex } from "@chakra-ui/react";
+import { Box, Center, ChakraProvider, Flex, Spinner } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { RecoilRoot } from "recoil";
 import "tailwindcss/tailwind.css";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function MyApp({ Component, pageProps: { session, ...pageProps }, router }: AppProps) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("findished");
+      setLoading(false);
+    };
+    router.events.on("routeChangeStart", start);
+    router.events.on("routeChangeComplete", end);
+    router.events.on("routeChangeError", end);
+    return () => {
+      router.events.off("routeChangeStart", start);
+      router.events.off("routeChangeComplete", end);
+      router.events.off("routeChangeError", end);
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -17,22 +40,36 @@ function MyApp({ Component, pageProps: { session, ...pageProps }, router }: AppP
         <RecoilRoot>
           <ChakraProvider>
             {router.route != "/" && (
-              <Box bgColor="whiteAlpha.900" position="sticky" top={0} zIndex={10} textColor="blackAlpha.900">
+              <Box
+                bgColor="whiteAlpha.900"
+                position="sticky"
+                top={0}
+                zIndex={10}
+                textColor="blackAlpha.900"
+              >
                 <Header />
               </Box>
             )}
 
-            {/* todo : fix landing page refresh scrolls down */}
-{/*             <ScaleFade key={router.route} initialScale={0.9} in={true}> */}
-              <div className="font-poppins font-bold">
-              <Flex minH="full" flexDirection="column" justifyContent="space-between">
-                <Component {...pageProps} />
-                {/* <CookieNotice /> */}
-                
-                <Footer />
-              </Flex>
-              </div>
-{/*             </ScaleFade> */}
+            <>
+              {loading ? (
+                <Center h={400}>
+                  <Spinner />
+                </Center>
+              ) : (
+                /*             <ScaleFade key={router.route} initialScale={0.9} in={true}> */
+                /* todo : fix landing page refresh scrolls down */
+                <div className="font-poppins font-bold">
+                  <Flex minH="full" flexDirection="column" justifyContent="space-between">
+                    <Component {...pageProps} />
+                    {/* <CookieNotice /> */}
+
+                    <Footer />
+                  </Flex>
+                </div>
+                /*             </ScaleFade> */
+              )}
+            </>
           </ChakraProvider>
         </RecoilRoot>
       </SessionProvider>
